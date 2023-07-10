cd2ae218c0a8467325c9aa562ad1274f0f07357a
VVV <VasilenkoVadim@mail.ru>
2023-07-10 11:12:11 +0300

    Дз №8. решение задач 54,56,58,60,62

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(n); // вызываем функцию для печати чисел
+
+
+static void PrintNumbers(int n) {
+    if (n == 0) {return;}
+    if (n == 1) { 
+        Console.Write(n);
+    } else { 
+        Console.Write(n + ", ");
+        PrintNumbers(n - 1);
+    }
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNaturalNumbers(m, n); // вызываем функцию для нахождения суммы
+
+if (sum == 0) {
+    Console.WriteLine("Введите другие числа, m должно быть меньше n");    
+}
+else {
+    Console.WriteLine("Сумма натуральных чисел от {0} до {1} равна {2}", m, n, sum);
+}
+
+static int SumNaturalNumbers(int m, int n) {
+    if (m > n) {return 0;}
+    if (m == n) {
+        return m;
+    } else { 
+        return m + SumNaturalNumbers(m + 1, n);
+    }
+}
